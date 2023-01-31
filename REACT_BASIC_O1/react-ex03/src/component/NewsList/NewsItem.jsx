0faf8f2afab82ef7f8e1c4873ca1698bc91feca4
gilyeon00 <12venus08@naver.com>
2023-01-31 16:52:13 +0900

    Refactor: props 구조분해할당

--- REACT_BASIC_O1/react-ex03/src/component/NewsList/NewsItem.jsx
+++ REACT_BASIC_O1/react-ex03/src/component/NewsList/NewsItem.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const NewsItem = ({topic, info, img}) => {
+const NewsItem = ({topic, info, img}) => {  // {props}를 풀때는 {{topic, img, info}} 이렇게 씀. 이름은 똑같아야함
     return (
         <li className='news-item'>
             <div className='item-img'>
