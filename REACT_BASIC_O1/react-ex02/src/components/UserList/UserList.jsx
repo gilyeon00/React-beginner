c153e84e425703d9f1ffcd4d417e44f3f26aea2c
gilyeon00 <12venus08@naver.com>
2023-01-31 12:27:25 +0900

    feat: userData에 있는 사진, 정보 화면에 뿌리기

--- REACT_BASIC_O1/react-ex02/src/components/UserList/UserList.jsx
+++ REACT_BASIC_O1/react-ex02/src/components/UserList/UserList.jsx
@@ -6,11 +6,23 @@ const UserList = () => {
     console.log(users)
     return (
         <div>
-            <li>
-                <img src="" alt=""/>
-                <span>{users[0].username}</span>
-                <span>online</span>
-            </li>
+            <ul>
+                {
+                    users.map((user) => {
+                        // {}쓰면 꼭 리턴해줘야하고, 한 줄로 할거면 ()로하고 return 안써도됨
+                        // map으로 생성하면 고유값이 부재하기 때문에 key값으로 고유값 부여하기! <- index번호말고 pk로 하기
+                        return (
+                            <li key={user.id}>
+                            <img src={user.picture} alt={user.name} />
+                            <span>{user.username}</span>
+                            <span>online</span>
+                        </li>
+                        )
+                    })
+                }
+
+            </ul>
+            
         </div>
     );
 };
