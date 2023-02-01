986f6e0d22921ecfdf294424eb6cb602b9be46fc
gilyeon00 <12venus08@naver.com>
2023-02-01 15:33:15 +0900

    design: props를 사용한 styled-component 예제

--- REACT_BASIC_02/react-ex07/src/component/Style.jsx
+++ REACT_BASIC_02/react-ex07/src/component/Style.jsx
@@ -5,14 +5,14 @@ const Style = () => {
     return (
         <div>
             <Box>
-                <Title>Styled Component</Title>
-                <Title>CSS IN JS</Title>
+                <Title fontSize="3rem" color="#def">Styled Component</Title>
+                <Title color="#daa">CSS IN JS</Title>
                 <Title>스타일드 컴포넌트</Title>
-                <Btn>normal</Btn>
-                <Btn>disabled</Btn>
-                <Btn>active</Btn>
+                <Btn borderColor='green'>normal</Btn>
+                <Btn borderColor='red'>disabled</Btn>
+                <Btn borderColor='yellow'>active</Btn>
                 <Btn>2023년 2월 1일</Btn>
-                <Text>2023년 2월 1일</Text>
+                <Text size="20px" color="#ccc">2023년 2월 1일</Text>
                 <Text>수요일</Text>
                 <Text>오후 3시</Text>
             </Box>
@@ -29,15 +29,26 @@ const Box = styled.div`
     width : 500px;
     height : 900px:
     background-color : rgb(255,242,555);
+    border : 2px solid black;
+    margin: 1rem;
 `
 
-const Btn = styled.button`
-`
-
+// props 사용
 const Title = styled.h2`
-    font-size : 2rem;
-    
+    font-size : ${(props) => props.fontSize};
+    color: ${(props) => props.color};
+    margin : 1rem 0;
 `
 
 const Text = styled.p`
+    font-size : ${(props) => props.size };
+    color : ${(props) => props.color};
+`
+
+const Btn = styled.button`
+    width: 200px;
+    height: 20px;
+    border: 1px solid ${(props) => props.borderColor};
+    background-color: #fff;
+
 `
