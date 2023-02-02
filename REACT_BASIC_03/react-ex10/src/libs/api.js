aa40809174b184ec4da4c672aabf3de69876e34d
gilyeon00 <12venus08@naver.com>
2023-02-02 18:19:02 +0900

    feat: youtube 클론코딩을 위한 api생성

---
+++ REACT_BASIC_03/react-ex10/src/libs/api.js
@@ -0,0 +1,11 @@
+const API_KEY = process.env.REACT_APP_GOOGLE_API_KEY
+
+export const videoURL = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=30&regionCode=kr&key=${API_KEY}`
+
+export function searchURL(keyword) {
+    return `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=30&q=${keyword}&regionCode=kr&type=video&key=${API_KEY}`
+}
+
+export function channelURL(channelID) {
+    return `https://youtube.googleapis.com/youtube/v3/channels?part=snippet&id=${channelID}&key=${API_KEY}`
+}
