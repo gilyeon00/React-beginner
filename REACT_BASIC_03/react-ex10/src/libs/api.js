af7b7d1cd2261e54a65b687e4bf0475a9b4e3c56
gilyeon00 <12venus08@naver.com>
2023-02-03 16:40:36 +0900

    feat: .env 이용해서 API_KEY 비공개

--- REACT_BASIC_03/react-ex10/src/libs/api.js
+++ REACT_BASIC_03/react-ex10/src/libs/api.js
@@ -1,5 +1,4 @@
-// const API_KEY = process.env.REACT_APP_GOOGLE_API_KEY
-const API_KEY = 'AIzaSyApI60Cgpj5iVn8Y0bIFXZWQFI0qxtm9AA';
+const API_KEY = process.env.REACT_APP_GOOGLE_API_KEY
 
 export const videoURL = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=30&regionCode=kr&key=${API_KEY}`
 
