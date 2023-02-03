3a3d38e6cce3391dee3e04a59027ad77017101eb
gilyeon00 <12venus08@naver.com>
2023-02-03 14:09:06 +0900

    fix: 검색안되던 오류해결 - 인자를 안넘겨줌

--- REACT_BASIC_03/react-ex10/src/pages/Search.jsx
+++ REACT_BASIC_03/react-ex10/src/pages/Search.jsx
@@ -3,14 +3,16 @@ import Sidebar from '../component/Sidebar';
 import VideoList from '../component/VideoList';
 import {videoURL, searchURL, channelURL} from './../libs/api'
 
-const Search = () => {
+const Search = ({videoItems, selectVideoItem}) => {
     return (
         <div className='contents-wrap'>
             <div>
                 <Sidebar />
             </div>
             <div className='video-list'>
-                <VideoList />
+                <VideoList 
+                    selectVideoItem = {selectVideoItem}
+                    videoItems = {videoItems} />
             </div>
         </div>
     );
