a7b97e17c46af2edd6000751c8d8aa6722f4cb23
gilyeon00 <12venus08@naver.com>
2023-02-06 16:12:56 +0900

    feat: 로그인 reducer 초기세팅

--- REACT_BASIC_03/react-ex11/src/store/index.js
+++ REACT_BASIC_03/react-ex11/src/store/index.js
@@ -1,12 +1,12 @@
 // store 세팅
 import { combineReducers, configureStore } from '@reduxjs/toolkit'
 import cartSlice from './cart/cartSlice'
-// import userSlice from './user/userSlice'
+import userSlice from './user/userSlice'
 
 // 여러 reducer을 사용할 때 
 const rootReducer = combineReducers({
     cart:cartSlice,
-    // user:userSlice
+    user:userSlice
 })
 
 const store = configureStore({
