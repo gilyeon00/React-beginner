40cc3774a87be973f0e73a7b9a3b9857fc048697
gilyeon00 <12venus08@naver.com>
2023-02-07 10:29:49 +0900

    feat: store 기본 세팅

--- REACT_BASIC_04/react-ex12/src/index.js
+++ REACT_BASIC_04/react-ex12/src/index.js
@@ -4,11 +4,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { Provider } from 'react';
+import store from './store/index'
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
+    <Provider store={store}>
     <App />
-  </React.StrictMode>
+    </Provider>
+  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ REACT_BASIC_04/react-ex12/src/store/list/listSlice.jsx
@@ -0,0 +1,20 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {
+  value: 0,
+}
+
+export const listSlice = createSlice({
+  name: 'list',
+  initialState,
+  reducers: {
+   
+
+
+  },
+})
+
+
+export const {  } = listSlice.actions
+
+export default listSlice.reducer
