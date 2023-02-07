3e726a31c7cccf16cdffb7917204e587778f59d7
gilyeon00 <12venus08@naver.com>
2023-02-07 10:16:04 +0900

    feat: CRUD를 위한 기본 세팅

---
+++ REACT_BASIC_04/react-ex12/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import ListItem from './component/ListItem';
+
+function App() {
+  return (
+    <div className="App">
+      <form>
+        <h1>글 목록</h1>
+        <div>
+          {/* <ListItem/> */}
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default App;
---
+++ REACT_BASIC_04/react-ex12/src/component/ListItem.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const ListItem = () => {
+    return (
+        <div>
+            <h2>아이디:컨텐츠</h2>
+            <input type="text" value=""/>
+            <button type="button">삭제</button>
+            <button type="button">수정</button>
+        </div>
+    );
+};
+
+export default ListItem;
